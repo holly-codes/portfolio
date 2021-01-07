2aab0b1c7fcd1f0069e6e6a7c40b7ee4718bcdbd
Holly Burinda <holly.burinda@gmail.com>
2021-01-07 18:03:48 -0500

    Remaining content

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,7 @@ import utils from "../styles/utils.module.css";
 import Navigation from "../components/navigation";
 import Footer from "../components/footer";
 import Head from "next/head";
+import styles from "../styles/Contact.module.css";
 
 function Contact() {
   return (
@@ -13,6 +14,20 @@ function Contact() {
 
       <main>
         <Navigation></Navigation>
+        <div className={styles.contactContent}>
+          <h1>Contact Me</h1>
+          <p>
+            Interested in working with me? I am currently off the market. But
+            I'm always looking to expand my professional network!
+          </p>
+          <p>
+            <em>Coming Soon: </em> a contact form! For now, email me at{" "}
+            <span className={utils.magicalUnderline}>
+              <a href="mailto:hello@holly-codes.com">hello@holly-codes.com</a>
+            </span>
+            .
+          </p>
+        </div>
       </main>
     </div>
   );
