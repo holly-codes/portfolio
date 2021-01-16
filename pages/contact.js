efb739a8149655d80561ce68f3bf6c405c93f805
Holly Burinda <holly.burinda@gmail.com>
2021-01-16 15:16:03 -0500

    Add favicon

--- pages/contact.js
+++ pages/contact.js
@@ -10,6 +10,23 @@ function Contact() {
       <Head>
         <title>Contact Me | Holly Codes</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <Navigation></Navigation>
       <main>
