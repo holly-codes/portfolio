4a234fb3a9b307ed0b34947fe5b2812c41ba25ec
Holly Burinda <holly.burinda@gmail.com>
2024-03-26 22:17:22 -0400

    Fix a11y issues picked up by linter

--- components/footer.jsx
+++ components/footer.jsx
@@ -8,16 +8,16 @@ const Footer = function() {
         <p>&copy; 2021 - Holly Burinda</p>
       </div>
       <div className={styles.footerLinks}>
-        <a href="https://github.com/holly-codes/">
+        <a href="https://github.com/holly-codes/" aria-label="GitHub">
           <i className="fab fa-github fa-2x" />
         </a>
-        <a href="https://codepen.io/holly-codes">
+        <a href="https://codepen.io/holly-codes" aria-label="CodePen">
           <i className="fab fa-codepen fa-2x" />
         </a>
-        <a href="https://www.linkedin.com/in/holly-burinda-33950964">
+        <a href="https://www.linkedin.com/in/holly-burinda-33950964" aria-label="LinkedIn">
           <i className="fab fa-linkedin-in fa-2x" />
         </a>
-        <a href="mailto:hello@holly-codes.com">
+        <a href="mailto:hello@holly-codes.com" aria-label="Email link to contact Holly">
           <i className="far fa-envelope fa-2x"/>
         </a>
       </div>
